d39cb039b46e0ff09d4063d128a0fe2bf96f9f22
SzylekPL <michalchrobak06@gmail.com>
2023-11-22 09:12:12 +0100

    Added a possibility to change InputChannel and ErrorDisplayChannel using delegates.

--- SafeParsers.cs
+++ SafeParsers.cs
@@ -10,102 +10,116 @@
 
 		public static bool Bool(InputChannel? inputChannel = null)
 		{
+			inputChannel ??= DefaultInputChannel;
 			bool i;
-			while (!bool.TryParse(Console.ReadLine(), out i))
+			while (!bool.TryParse(inputChannel(), out i))
 				DefaultErrorDisplayChannel();
 			return i;
 		}
 		public static byte Byte(InputChannel? inputChannel = null)
 		{
+			inputChannel ??= DefaultInputChannel;
 			byte i;
-			while (!byte.TryParse(Console.ReadLine(), out i))
+			while (!byte.TryParse(inputChannel(), out i))
 				DefaultErrorDisplayChannel();
 			return i;
 		}
 		public static sbyte SByte(InputChannel? inputChannel = null)
 		{
+			inputChannel ??= DefaultInputChannel;
 			sbyte i;
-			while (!sbyte.TryParse(Console.ReadLine(), out i))
+			while (!sbyte.TryParse(inputChannel(), out i))
 				DefaultErrorDisplayChannel();
 			return i;
 		}
 		public static short Short(InputChannel? inputChannel = null)
 		{
+			inputChannel ??= DefaultInputChannel;
 			short i;
-			while (!short.TryParse(Console.ReadLine(), out i))
+			while (!short.TryParse(inputChannel(), out i))
 				DefaultErrorDisplayChannel();
 			return i;
 		}
 		public static ushort UShort(InputChannel? inputChannel = null)
 		{
+			inputChannel ??= DefaultInputChannel;
 			ushort i;
-			while (!ushort.TryParse(Console.ReadLine(), out i))
+			while (!ushort.TryParse(inputChannel(), out i))
 				DefaultErrorDisplayChannel();
 			return i;
 		}
 		public static int Int(InputChannel? inputChannel = null)
 		{
+			inputChannel ??= DefaultInputChannel;
 			int i;
-			while (!int.TryParse(Console.ReadLine(), out i))
+			while (!int.TryParse(inputChannel(), out i))
 				DefaultErrorDisplayChannel();
 			return i;
 		}
 		public static uint UInt(InputChannel? inputChannel = null)
 		{
+			inputChannel ??= DefaultInputChannel;
 			uint i;
-			while (!uint.TryParse(Console.ReadLine(), out i))
+			while (!uint.TryParse(inputChannel(), out i))
 				DefaultErrorDisplayChannel();
 			return i;
 		}
 		public static long Long(InputChannel? inputChannel = null)
 		{
+			inputChannel ??= DefaultInputChannel;
 			long i;
-			while (!long.TryParse(Console.ReadLine(), out i))
+			while (!long.TryParse(inputChannel(), out i))
 				DefaultErrorDisplayChannel();
 			return i;
 		}
 		public static ulong ULong(InputChannel? inputChannel = null)
 		{
+			inputChannel ??= DefaultInputChannel;
 			ulong i;
-			while (!ulong.TryParse(Console.ReadLine(), out i))
+			while (!ulong.TryParse(inputChannel(), out i))
 				DefaultErrorDisplayChannel();
 			return i;
 		}
 		public static float Double(InputChannel? inputChannel = null)
 		{
+			inputChannel ??= DefaultInputChannel;
 			float i;
-			while (!float.TryParse(Console.ReadLine(), out i))
+			while (!float.TryParse(inputChannel(), out i))
 				DefaultErrorDisplayChannel();
 			return i;
 		}
 		public static double Float(InputChannel? inputChannel = null)
 		{
+			inputChannel ??= DefaultInputChannel;
 			double i;
-			while (!double.TryParse(Console.ReadLine(), out i))
+			while (!double.TryParse(inputChannel(), out i))
 				DefaultErrorDisplayChannel();
 			return i;
 		}
 		public static decimal Decimal(InputChannel? inputChannel = null)
 		{
+			inputChannel ??= DefaultInputChannel;
 			decimal i;
-			while (!decimal.TryParse(Console.ReadLine(), out i))
+			while (!decimal.TryParse(inputChannel(), out i))
 				DefaultErrorDisplayChannel();
 			return i;
 		}
 		public static char Char(InputChannel? inputChannel = null)
 		{
+			inputChannel ??= DefaultInputChannel;
 			char i;
-			while (!char.TryParse(Console.ReadLine(), out i))
+			while (!char.TryParse(inputChannel(), out i))
 				DefaultErrorDisplayChannel();
 			return i;
 		}
 		public static string String(InputChannel? inputChannel = null)
 		{
-			string i = Console.ReadLine();
+			inputChannel ??= DefaultInputChannel;
+			string i = inputChannel();
 			while (string.IsNullOrEmpty(i.Trim()))
 			{
 				DefaultErrorDisplayChannel();
-				i = Console.ReadLine();
+				i = inputChannel();
 			}
 			return i;
 		}
